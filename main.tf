69e367823d00dfc2a961b20ca051f62f7084673b
ml4 <ml4@psy.industries>
2020-06-01 17:23:36 +0100

    Fix router name

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "google_compute_address" "hcNatIp" {
 
 resource "google_compute_router_nat" "hcNgw" {
   name                               = "${var.prefix}-ngw"
-  router                             = google_compute_router.hcRtrPublic.googleProject
+  router                             = google_compute_router.hcRtr.googleProject
   nat_ip_allocate_option             = "MANUAL_ONLY"
   nat_ips                            = ["${google_compute_address.hcNatIp.*.self_link}"]
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
