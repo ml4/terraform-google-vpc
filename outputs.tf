e76532432fa1aeebdc2577fdf63f0e7263289998
ml4 <ml4@psy.industries>
2020-06-09 12:07:12 +0100

    Test concat in outputs

--- outputs.tf
+++ outputs.tf
@@ -30,7 +30,8 @@ output "vpc-gcn-hcVpc-gatewayIpv4" {
 ## private subnet
 #
 output "vpc-gcsn-hcPrivate-ipCidrRange" {
-  value = google_compute_subnetwork.hcPrivate[*].ip_cidr_range
+  # value = google_compute_subnetwork.hcPrivate[*].ip_cidr_range
+  value = concat(google_compute_subnetwork.hcPrivate[*].ip_cidr_range, [""])[0]
 }
 
 # output "vpc-gcsn-hcPrivate-name" {
