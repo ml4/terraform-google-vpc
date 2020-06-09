ea1a8b56b48b5ec4762ec03fbbe2a688375d0be4
ml4 <ml4@psy.industries>
2020-06-09 12:23:12 +0100

    Test outputs with single list accessor

--- outputs.tf
+++ outputs.tf
@@ -30,7 +30,7 @@ output "vpc-gcn-hcVpc-gatewayIpv4" {
 ## private subnet
 #
 output "vpc-gcsn-hcPrivate-ipCidrRange" {
-  value = google_compute_subnetwork.hcPrivate[*].ip_cidr_range
+  value = google_compute_subnetwork.hcPrivate[0].ip_cidr_range
 }
 
 # output "vpc-gcsn-hcPrivate-name" {
