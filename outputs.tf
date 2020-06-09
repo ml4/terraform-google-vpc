9b39facaafd58959e8d9fa4a9e7ce40a6ea62b4b
ml4 <ml4@psy.industries>
2020-06-09 13:27:37 +0100

    Test list hash output access

--- outputs.tf
+++ outputs.tf
@@ -30,7 +30,7 @@ output "vpc-gcn-hcVpc-gatewayIpv4" {
 ## private subnet
 #
 output "vpc-gcsn-hcPrivate-ipCidrRange" {
-  value = google_compute_subnetwork.hcPrivate[0][*]
+  value = google_compute_subnetwork.hcPrivate[*][*]["ip_cidr_range"]
   # value = [
   #   for cidr in google_compute_subnetwork.hcPrivate[*]:
   #   cidr[0]
