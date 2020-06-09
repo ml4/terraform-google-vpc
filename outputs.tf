b247522940e973ee0f697eb32929a0f10c40517f
ml4 <ml4@psy.industries>
2020-06-09 12:42:16 +0100

    Go back to the previous working model of outputs

--- outputs.tf
+++ outputs.tf
@@ -30,7 +30,10 @@ output "vpc-gcn-hcVpc-gatewayIpv4" {
 ## private subnet
 #
 output "vpc-gcsn-hcPrivate-ipCidrRange" {
-  value = google_compute_subnetwork.hcPrivate[*].cidr
+  value = [
+    for cidr in google_compute_subnetwork.hcPrivate[*]:
+    cidr.ip_cidr_range
+  ]
 }
 
 # output "vpc-gcsn-hcPrivate-name" {
