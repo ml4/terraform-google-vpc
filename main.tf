76bc246527fb936910e924ed7559cacd0c4e332d
ml4 <ml4@psy.industries>
2020-06-10 09:10:09 +0100

    Add tff

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "google_compute_router_nat" "hcNgw" {
   router                             = google_compute_router.hcRtr.name
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-  depends_on                         = [
+  depends_on = [
     google_compute_address.hcNatIp
   ]
 }
