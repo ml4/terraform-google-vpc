117a7eb5dd74be54ef61d3f97c83cd59cb44d2fe
ml4 <ml4@psy.industries>
2020-06-13 19:55:02 +0100

    Add credence to Tom S and statsd port

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 ## GCP VPC
-## Credence to Alex B.
+## Credence to Alex Basista & Tom Straub
 #
 resource "google_compute_network" "hcVpc" {
   name                            = "${var.prefix}-vpc"
@@ -34,10 +34,17 @@ resource "google_compute_firewall" "hcFW" {
   network = google_compute_network.hcVpc.self_link
 
   ## include port 80 for the certbot acme webserver to automatically get certs
+  ## also use port 80 for the TFE ES mode fw-allow-health-check rule: https://cloud.google.com/load-balancing/docs/https/ext-http-lb-simple#firewall (130.211.0.0/22 and 35.191.0.0/16 implicit in the 0.0.0.0/0 below)
+  ## 23010 for statsd
+  ## 8800 for replicated ui
   #
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "443", "8800"]
+    ports    = ["22", "80", "443", "8800", "23010"]
+  }
+
+  allow {
+    protocol = "icmp"
   }
 
   source_ranges = ["0.0.0.0/0"]
