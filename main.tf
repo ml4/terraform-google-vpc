61b6a2c788621eae80a12dfc0d2502fcf13ea2fa
ml4 <ml4@psy.industries>
2021-10-14 22:19:44 +0100

    Refactor comments

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-//// GCP VPC
-//// Credence to Alex Basista & Tom Straub
-//
+## GCP VPC
+## Credence to Alex Basista & Tom Straub
+#
 resource "google_compute_network" "main" {
   name                            = "${var.prefix}-vpc"
   auto_create_subnetworks         = false
@@ -8,8 +8,8 @@ resource "google_compute_network" "main" {
   delete_default_routes_on_create = false
 }
 
-//// subnets
-//
+## subnets
+#
 resource "google_compute_subnetwork" "primary_public" {
   for_each = var.primary_public_subnet_cidrs
 
@@ -48,17 +48,17 @@ resource "google_compute_subnetwork" "secondary_private" {
   region                   = var.google_secondary_region
 }
 
-//// FWs
-//
+## FWs
+#
 resource "google_compute_firewall" "main" {
   name    = "${var.prefix}-${google_compute_network.main.name}-fw"
   network = google_compute_network.main.self_link
 
-  //// include port 80 for the certbot acme webserver to automatically get certs
-  //// also use port 80 for the TFE ES mode fw-allow-health-check rule: https://cloud.google.com/load-balancing/docs/https/ext-http-lb-simple//firewall (130.211.0.0/22 and 35.191.0.0/16 implicit in the 0.0.0.0/0 below)
-  //// 23010 for statsd
-  //// 8800 for replicated ui
-  //
+  ## include port 80 for the certbot acme webserver to automatically get certs
+  ## also use port 80 for the TFE ES mode fw-allow-health-check rule: https://cloud.google.com/load-balancing/docs/https/ext-http-lb-simple//firewall (130.211.0.0/22 and 35.191.0.0/16 implicit in the 0.0.0.0/0 below)
+  ## 23010 for statsd
+  ## 8800 for replicated ui
+  #
   allow {
     protocol = "tcp"
     ports    = ["22", "80", "443", "8800", "23010"]
@@ -71,8 +71,8 @@ resource "google_compute_firewall" "main" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-//// primary routing
-//
+## primary routing
+#
 resource "google_compute_router" "primary_rtr" {
   name    = "${var.prefix}-primary-rtr"
   network = google_compute_network.main.self_link
@@ -98,8 +98,8 @@ resource "google_compute_router_nat" "primaryNgw" {
   ]
 }
 
-//// secondary routing
-//
+## secondary routing
+#
 resource "google_compute_router" "secondary_rtr" {
   name    = "${var.prefix}-secondary-rtr"
   network = google_compute_network.main.self_link
